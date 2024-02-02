003bd98e11aa01849da32089bac2b2e9d6b763c0
Pisarski Roman <romanooo84@gmail.com>
2024-02-02 18:36:24 +0100

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// Opisany w dokumentacji
+import SimpleLightbox from "simplelightbox";
+// Dodatkowy import stylów
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+
+const galleryContainer = document.querySelector(".gallery");
+//mapuje images i tworzę ciąg znaków z kodem html. Dla src i alt pobieram dane z tabllicy images. join łaczy tablice w ciąg znaków
+const markup = galleryItems.map(image =>
+	`<a class="gallery__item" href='${image.original}'>
+        <img class="gallery__image"
+            src='${image.preview}'
+            alt='${image.description}' />
+    </a>`)
+	.join('');
+//tworzy elementy html wylistowane w markup
+galleryContainer.insertAdjacentHTML("beforeend", markup);
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    animationSpeed: 750,
+    enableKeyboard: true,
+    loop: true,
+});
+    lightbox.on()
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
+import Player from '@vimeo/player';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+
+
+    
+
 
