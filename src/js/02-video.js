32f031e74c951644068fffd9943ce9ba66113dc5
Pisarski Roman <romanooo84@gmail.com>
2024-02-02 18:37:08 +0100

    a

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,7 +3,13 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+localStorage.clear()
 
+
+player.on('timeupdate', function (data) {
+    localStorage.clear()
+         
+});
 
     
 
