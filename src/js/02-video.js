6f05f391311d66725926743e646b9aecd9713d87
Pisarski Roman <romanooo84@gmail.com>
2024-02-03 07:19:34 +0100

    set

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,13 +3,18 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-localStorage.clear()
 
+let time = localStorage.getItem('videoplayer-stop-time')
+
+player.setCurrentTime(time)
 
 player.on('timeupdate', function (data) {
-    localStorage.clear()
-         
+    localStorage.setItem('videoplayer-current-time', data.seconds)
 });
+
+player.on('pause', function (data) {
+    localStorage.setItem('videoplayer-stop-time',data.seconds)
+})
 
     
 
