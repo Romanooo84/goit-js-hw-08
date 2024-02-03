dffc0e1a1de9c58fe2628414819ddb93bc5f786a
Pisarski Roman <romanooo84@gmail.com>
2024-02-03 13:13:29 +0100

    zad 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -29,8 +29,9 @@ let delay= throttle(getLocalStorageData, 500);
 // nasłuchowanie pól email i message i uruchomienie funkcji zapisywanie pół do local storage
 emailtInput.addEventListener('input', delay);
 messageInput.addEventListener('input', delay);
-submitButton.addEventListener("click", function clear () {
+
+// czyszczenie pola email i message w local storage po wcisnieciu submit
+submitButton.addEventListener("click", clear => {
     localStorage.removeItem("email");
     localStorage.removeItem("message");
-    console.log('cleared')
 })
