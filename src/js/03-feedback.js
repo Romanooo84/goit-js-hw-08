93247f4dc003e8b8eb34f38c7490d4af4e1e3fa6
Pisarski Roman <romanooo84@gmail.com>
2024-02-04 11:51:00 +0100

    change

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,29 +1,58 @@
 import throttle from 'lodash.throttle';
 
-//wyszukiwanie pól email i text
-let emailtInput = document.querySelector("input[type='email']");
-let messageInput = document.querySelector("textarea[name='message']");
-const submitButton= document.querySelector("button[type='submit']");
- 
-//pobieranie danych z local storage dla email i message
-let dataEmail = localStorage.getItem("email")
-let dataMessage = localStorage.getItem("message");
+//wyszukiwanie pół form
+let dataInput = document.querySelector(".feedback-form");
 
-//wstawianie w pole email i message wartości z local storage
-emailtInput.value = dataEmail
-messageInput.value = dataMessage;
+//pobieranie danych z lokalstorage
+let feedbackFormState = localStorage.getItem('feedback-form-state'))
 
-//funkcja ustawiająca wartości email i message w lockal storage
-function getLocalStorageData() {
-    const valueEmail = emailtInput.value
-    const valueMessage = messageInput.value
-        
-    localStorage.setItem("email", valueEmail)
-    localStorage.setItem("message", valueMessage)
-    
+//sprawdzanie czy są wpisane dane do local sotrage
+if (feedbackFormState == undefined) {
+    feedbackFormState = {
+        email: "",
+        message: "",
+    }
+} else {
+    //ponieranie danych z lockalsorage i prasowanie
+    feedbackFormState = JSON.parse(feedbackFormState);
+    //wstawianie danych do pół input i textarea
+    let email = document.querySelector('input');
+    let message = document.querySelector('textarea');
+    email.value = feedbackFormState.email;
+    message.value = feedbackFormState.message;
+}
+console.log(feedbackFormState)
+
+// funkcja zapisywania danych do local storage po pojawieniu sie zdarzenia
+function formInput(event) {
+    //zapisanie wartosci zdarzenia
+    const dataValue = event.target.value
+    //zapisanie elementu html i ustawienie małych liter
+    const dataName = event.target.nodeName.toLowerCase() 
+    //sprawdzenie rodzaju elementu HTML i przypisanie do wartosci zdarzenia do odpowiedniego klucza
+    if (dataName === "input") {
+        feedbackFormState.email = dataValue;
+        //przetwarzanie na JSON
+        feedbackFormStateJSON = JSON.stringify(feedbackFormState)
+        //zapisywanie w localstorage
+        localStorage.setItem('feedback-form-state',feedbackFormStateJSON )
+    }
+    else if (dataName === "textarea") {
+        feedbackFormState.message = dataValue;
+        feedbackFormStateJSON = JSON.stringify(feedbackFormState)
+        localStorage.setItem('feedback-form-state',feedbackFormStateJSON )
+    }
+    else {
+        localStorage.removeItem('feedback-form-state')
+    }
+    console.log(feedbackFormState)
 }
 
 // ustawienie opóźnienia dla wykonywanie funkcji getLocalStorageData
-let delay= throttle(getLocalStorageData, 500);
+let delay = throttle(formInput, 500);
+
+//nasłuchiwanie wpisywania i kliknięć
+dataInput.addEventListener("input", delay)
+dataInput.addEventListener("click", delay)
 
 
