41f11399d3e4eb8424f6b2a714de27cd136bde10
Pisarski Roman <romanooo84@gmail.com>
2024-02-05 22:45:29 +0100

    change

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,6 @@
 import throttle from 'lodash.throttle';
 //inicjacja zmiennej w formacie JSONasd
-let feedbackFormStateJSON 
+let feedbackFormStateJSON
 
 //wyszukiwanie pół form
 let dataInput = document.querySelector(".feedback-form");
@@ -21,33 +21,37 @@ if (feedbackFormState == undefined) {
     let email = document.querySelector('input');
     let message = document.querySelector('textarea');
     email.value = feedbackFormState.email;
-    message.value = feedbackFormState.message;
+    message.value = feedbackFormState.message.trim();
 }
-console.log(feedbackFormState)
+
 
 // funkcja zapisywania danych do local storage po pojawieniu sie zdarzenia
 function formInput(event) {
     //zapisanie wartosci zdarzenia
     const dataValue = event.target.value
     //zapisanie elementu html i ustawienie małych liter
-    const dataName = event.target.nodeName.toLowerCase() 
+    const dataName = event.target.nodeName.toLowerCase()
     //sprawdzenie rodzaju elementu HTML i przypisanie do wartosci zdarzenia do odpowiedniego klucza
     if (dataName === "input") {
-        feedbackFormState.email = dataValue;
+        feedbackFormState.email = dataValue.trim();
         //przetwarzanie na JSON
         feedbackFormStateJSON = JSON.stringify(feedbackFormState)
         //zapisywanie w localstorage
-        localStorage.setItem('feedback-form-state',feedbackFormStateJSON )
+        localStorage.setItem('feedback-form-state', feedbackFormStateJSON)
     }
     else if (dataName === "textarea") {
-        feedbackFormState.message = dataValue;
+        feedbackFormState.message = dataValue.trim();
         feedbackFormStateJSON = JSON.stringify(feedbackFormState)
-        localStorage.setItem('feedback-form-state',feedbackFormStateJSON )
+        localStorage.setItem('feedback-form-state', feedbackFormStateJSON)
     }
     else {
         localStorage.removeItem('feedback-form-state')
-    }
-    console.log(feedbackFormState)
+    }  
+}
+
+//funkcja wyswietlania wprowadzonych danych w konsoli
+function focus() {
+    console.log(feedbackFormState);
 }
 
 // ustawienie opóźnienia dla wykonywanie funkcji getLocalStorageData
@@ -56,5 +60,5 @@ let delay = throttle(formInput, 500);
 //nasłuchiwanie wpisywania i kliknięć
 dataInput.addEventListener("input", delay)
 dataInput.addEventListener('submit', delay)
-
+dataInput.addEventListener('change', focus)
 
